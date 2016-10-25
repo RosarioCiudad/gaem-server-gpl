dc655ef028b72cbc5d7449683c01ff36c996cc4a
Agustin Rios <agustin.rios@tecso.coop>
2016-10-25 16:02:35 -0300

    MRGAIT-463 pasar login de get a post

--- buss/src/ar/gov/rosario/gait/seg/buss/service/LoginResource.java
+++ buss/src/ar/gov/rosario/gait/seg/buss/service/LoginResource.java
@@ -34,20 +34,23 @@ public class LoginResource {
 
 	private Logger log = Logger.getLogger(LoginResource.class);
 	private To to = new To();
-	
+
 	public Route[] routes() {
 		return new Route[] {
-				Route.create("GET", "/gait/api/login.json", this.getClass(), "login")
+				Route.create("POST", "/gait/api/loginManager.json", this.getClass(), "loginPost"),
+				Route.create("GET", "/gait/api/login.json", this.getClass(), "loginGet")
 		};
 	}
 
 	/**
 	 * 
 	 */
-	public Reply<UsuarioApmVO> login(RestRequest<String> req) throws Exception {
+	public Reply<UsuarioApmVO> loginPost(RestRequest<String> req) throws Exception {
 		String funcName = DemodaUtil.currentMethodName();
 		if (log.isDebugEnabled()) log.debug(funcName + ": enter");
-		
+
+		log.debug("LOGIN: entro a loginPost()");
+
 		String username = to.String(req.parameters.get("username"));
 		String password = to.String(req.parameters.get("password"));
 		String deviceid = to.String(req.parameters.get("deviceid"));
@@ -55,7 +58,38 @@ public class LoginResource {
 		try {
 			// Obtengo certificado y token
 			UsuarioApmVO usuarioApmVO = SegSeguridadManager.loginUsuarioApm(username, password, deviceid);
-			
+
+			if(usuarioApmVO.hasError()){
+				String msg = usuarioApmVO.getListError().get(0).key().substring(1);
+				return new Reply<UsuarioApmVO>(500, msg, new UsuarioApmVO());
+			}
+			return new Reply<UsuarioApmVO>(usuarioApmVO);
+		} catch (Exception e) {
+			throw new DemodaServiceException(e);
+		} finally {
+			GaitHibernateUtil.closeSession();
+		}
+	}
+
+	/**
+	 * En un principio el login se hacia por post, en la versión nueva de la aplicación para el moto E se cambió para que sea por post.
+	 * Se mantiene este método por la compatibilidad hacia atrás, para que los celulares con la app vieja sigan podiendo conectarse al server.
+	 */
+	@Deprecated
+	public Reply<UsuarioApmVO> loginGet(RestRequest<String> req) throws Exception {
+		String funcName = DemodaUtil.currentMethodName();
+		if (log.isDebugEnabled()) log.debug(funcName + ": enter");
+
+		log.debug("LOGIN: entro a loginGet()");
+
+		String username = to.String(req.parameters.get("username"));
+		String password = to.String(req.parameters.get("password"));
+		String deviceid = to.String(req.parameters.get("deviceid"));
+
+		try {
+			// Obtengo certificado y token
+			UsuarioApmVO usuarioApmVO = SegSeguridadManager.loginUsuarioApm(username, password, deviceid);
+
 			if(usuarioApmVO.hasError()){
 				String msg = usuarioApmVO.getListError().get(0).key().substring(1);
 				return new Reply<UsuarioApmVO>(500, msg, new UsuarioApmVO());
